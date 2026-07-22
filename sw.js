2dd62f782713723990134dd1e2e05db93bb5e6b5
merkos1969 <merkos1969@gmail.com>
2026-07-22 17:00:58 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="matchvision-v80";
+const CACHE_NAME="matchvision-v82";
 const APP_SHELL=["./","./index.html","./manifest.webmanifest","./icon.svg","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()));
@@ -13,13 +13,11 @@ self.addEventListener("fetch",event=>{
     event.respondWith(fetch(event.request));
     return;
   }
-  event.respondWith(
-    fetch(event.request)
-      .then(response=>{
-        const copy=response.clone();
-        caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy));
-        return response;
-      })
-      .catch(()=>caches.match(event.request).then(r=>r||caches.match("./index.html")))
-  );
+  event.respondWith(fetch(event.request)
+    .then(response=>{
+      const copy=response.clone();
+      caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy));
+      return response;
+    })
+    .catch(()=>caches.match(event.request).then(r=>r||caches.match("./index.html"))));
 });
