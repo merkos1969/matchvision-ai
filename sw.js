9fd21a3f9c1f60b2f9925a57c1c3cf151b7dc7b0
merkos1969 <merkos1969@gmail.com>
2026-07-22 23:22:25 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="matchvision-v84";
+const CACHE_NAME="matchvision-v85";
 const APP_SHELL=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()));
